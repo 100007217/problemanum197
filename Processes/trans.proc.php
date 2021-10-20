f493b2ae2d09703cc5242789d701e7e0bb6c63ee
100007217 <gomezmonterroso14@gmail.com>
2021-10-20 18:25:06 +0200

    Desarrollo hasta el primer dia

--- Processes/trans.proc.php
+++ Processes/trans.proc.php
@@ -1,11 +1,36 @@
 <?php
     $frase=$_POST['frase'];
+    $vocalesstring="aeiou";
     echo $frase. "<br>";
 
-    $chars = str_split($frase);
- 
-    foreach ($chars as $char) {
-        echo $char;
+    $frasefinal= "";
+
+    $frasecaracteres = str_split($frase);
+    $vocales = str_split($vocalesstring);
+    $numcaracteres = strlen($frase);
+    echo "Numero de caracteres es ".$numcaracteres. "<br></br>";
+
+
+
+    $contadorcons=0;
+    for ($i=0; $i < $numcaracteres; $i++){ 
+        echo $frasecaracteres[$i];
+
+        if(in_array($frasecaracteres[$i], $vocales)){
+            echo " Encontré vocal";
+            if ($contadorcons>1) {
+                
+            }else {
+                $frasefinal=$frasefinal.$frasecaracteres[$i];
+            }
+            $contadorcons=0;
+        }else {
+            echo " Encontré una consonante";
+            $contadorcons=$contadorcons+1;
+            $frasefinal=$frasefinal.$frasecaracteres[$i];
+        }
         echo "<br>";
-    }
+        echo $frasefinal."<br>";
+        echo "<br></br>";
+    }                                                                                                                                                    n
 ?>
