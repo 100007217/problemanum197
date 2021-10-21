367b26a6c417287d2dabe5081b1472097c8a1006
100007217 <gomezmonterroso14@gmail.com>
2021-10-21 17:31:14 +0200

    Ejercicio hecho

--- Processes/trans.proc.php
+++ Processes/trans.proc.php
@@ -1,9 +1,10 @@
 <?php
     $frase=$_POST['frase'];
-    $vocalesstring="aeiou";
+    $vocalesstring="aeiouAEIOU";
     echo $frase. "<br>";
 
     $frasefinal= "";
+    $stringcons="";
 
     $frasecaracteres = str_split($frase);
     $vocales = str_split($vocalesstring);
@@ -14,23 +15,45 @@
 
     $contadorcons=0;
     for ($i=0; $i < $numcaracteres; $i++){ 
-        echo $frasecaracteres[$i];
+        echo "Letra actual ".$frasecaracteres[$i];
 
         if(in_array($frasecaracteres[$i], $vocales)){
-            echo " Encontré vocal";
+            echo " -> Encontré vocal";
+
             if ($contadorcons>1) {
+                //Añadimos la consonantes especularmente
+                $strinconsreversed=strrev($stringcons);
+                $frasefinal=$frasefinal.$strinconsreversed;
+                $frasefinal=$frasefinal.$frasecaracteres[$i];
+                $strinconsreversed="";
                 
             }else {
+                //Añadimos la unica consonante anterior
+                $frasefinal=$frasefinal.$stringcons;
+                //AÑadimos la vocal que toque
                 $frasefinal=$frasefinal.$frasecaracteres[$i];
+                echo "<br>".$frasefinal;
             }
+            //Reseteamos el contador de consonantes y sus strings
             $contadorcons=0;
+            $stringcons="";
         }else {
-            echo " Encontré una consonante";
+            echo " ->  Encontré una consonante";
             $contadorcons=$contadorcons+1;
-            $frasefinal=$frasefinal.$frasecaracteres[$i];
+            $stringcons=$stringcons.$frasecaracteres[$i];
+            echo "<br>Es la linea de consonantes actual: ".$stringcons;
         }
         echo "<br>";
-        echo $frasefinal."<br>";
+        echo "<br>Frase final por ahora: ".$frasefinal;
         echo "<br></br>";
-    }                                                                                                                                                    n
+    }   
+    //Ultima ejecucion por si hay alguna consonante
+    if ($contadorcons=0) {
+        echo "No hay ninguna cononante ya";
+    } else {
+        $strinconsreversed=strrev($stringcons);
+        $frasefinal=$frasefinal.$strinconsreversed;
+    }
+    
+    echo "<br>Frase final FINAL: ".$frasefinal."<br>";                                                                                                                                            n
 ?>
